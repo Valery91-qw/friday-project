4e59e2dd7c208036d5da2d0d1542914ad1f920d8
Valery91-qw <vakriv91@gmail.com>
2021-06-05 16:52:40 +0300

    add create and delete cardAPI create delete and create CardThunk

--- src/components/packs/Packs.tsx
+++ src/components/packs/Packs.tsx
@@ -6,12 +6,17 @@ import style from "./Packs.module.scss"
 import {CustomButton} from "../../Common/CustomElements/Button/CustomButton";
 import {PacksTable} from "./table/PacksTable";
 import {Pagination} from "../../Common/Pagination/Pagination";
+import {useEffect} from "react";
 
 export const Packs = () => {
 
     const packs = useSelector<RootStateType, Array<PackType> | null>(state => state.packs.cardPacks)
     const {cardPacksTotalCount = 0, pageCount = 0, page = 0} = useSelector<RootStateType, any>(state => state.packs)
     const dispatch = useDispatch()
+    
+    useEffect(() => {
+        dispatch(getPacks())
+    }, [dispatch])
 
     return (<div className={style.tableWrapper}>
         <PacksTable packs={packs}/>
--- src/components/packs/cards/card/Card.tsx
+++ src/components/packs/cards/card/Card.tsx
@@ -1,16 +1,28 @@
 import {CardType} from "../../../../dal/cards";
 import React, {FC} from "react";
 import style from "./Card.module.scss"
+import {useDispatch} from "react-redux";
+import {deleteCard} from "../../../../bll/cards-reducer";
 
 type PropsType = {
     card: CardType
+    currentPackId: string | null
 }
 
-export const Card: FC<PropsType> = ({card}) => {
+export const Card: FC<PropsType> = ({card, currentPackId}) => {
+
+    const dispatch = useDispatch()
+
+    const handleDelete = () => {
+        if(currentPackId)
+        dispatch(deleteCard(card._id, currentPackId))
+    }
+
     return (
         <div className={style.cardWrapper}>
             <div>
                 Question: {card.question}
+                <button onClick={handleDelete}>delete</button>
             </div>
         </div>
     )
